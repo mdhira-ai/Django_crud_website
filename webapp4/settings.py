7c7a75a263e5b552e256ccc6078c74fc34f955da
MD HABIBOR RAHMAN HIRA <68804057+mdhira-ai@users.noreply.github.com>
2021-03-25 21:15:38 -0500

    Update settings.py

--- webapp4/settings.py
+++ webapp4/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '1jr+3*59o0zm27gfh!j*e5fbb=6ygnd(ml6jqs49f7h@q83oj#'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
